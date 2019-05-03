7ca446502fc81a8b39a19af30bd8afc488463e72
Liam Johnson <liamjejohnson@gmail.com>
2019-05-03 17:05:59 +0100

    buttons added and some styling done

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { LinearGradient } from "expo";
 import { Input, Button } from "react-native-elements";
 
 export default class App extends React.Component {
   render() {
     return (
-      <LinearGradient colors={["#6BF755", "#0E9D22"]} style={{ flex: 1 }}>
+      <LinearGradient colors={["#a2ea54", "#669335"]} style={{ flex: 1 }}>
         <View style={styles.container}>
+          <Image
+            style={{
+              width: 220,
+              height: 95,
+              marginBottom: 50,
+              marginRight: 75
+            }}
+            source={require("./assets/white-leaf-png-6.png")}
+          />
           <Input placeholder="Username" />
           <Input placeholder="Password" />
-          <Button title="Log In" buttonStyle={{ width: "100%" }} />
-          <Button title="Sign Up" />
+          <Button
+            title="Log In"
+            buttonStyle={{
+              width: "150%",
+              margin: "6%",
+              backgroundColor: "white"
+            }}
+            titleStyle={{ color: "black" }}
+          />
+          <Button
+            title="Sign Up"
+            buttonStyle={{ width: "150%", backgroundColor: "white" }}
+            titleStyle={{ color: "black", textAlign: "center" }}
+          />
         </View>
       </LinearGradient>
     );
