afb722e759f83cf68647e4173f31210b9c36a02b
Liam Johnson <liamjejohnson@gmail.com>
2019-05-03 16:18:46 +0100

    testing ui components

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo";
+import { Input, Button } from "react-native-elements";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.input}>
-          <Text>Log in</Text>
-          <TextInput placeholder="username" />
-          <TextInput placeholder="password" />
+      <LinearGradient colors={["#6BF755", "#0E9D22"]} style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Button title="Log In" buttonStyle={{ width: "100%" }} />
+          <Button title="Sign Up" />
         </View>
-      </View>
+      </LinearGradient>
     );
   }
 }
@@ -18,17 +21,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  },
-  input: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: "red",
-    borderStyle: "solid",
-    borderWidth: 2,
-    height: 100,
-    width: "100%"
   }
 });
